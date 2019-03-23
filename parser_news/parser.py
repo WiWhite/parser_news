72529fcdac4cc56eed0015caf936aed8185e8edc
Alexander Slipchenko <bigsplash1990@gmail.com>
2019-03-23 18:35:21 +0200

    read html and wr news in file

--- parser_news/parser.py
+++ parser_news/parser.py
@@ -13,7 +13,10 @@ def get_all_links(html):
     domen = 'https://strana.ua'
     soup = BeautifulSoup(html, 'lxml')
 
-    titles = soup.find('div', {'class': 'lenta'}).find_all('div', {'class': 'title'})
+    titles = soup.find('div', {'class': 'lenta'}).find_all(
+                                                      'div',
+                                                      {'class': 'title'}
+    )
 
     links = []
 
@@ -24,11 +27,34 @@ def get_all_links(html):
 
     return links
 
+
+def get_links_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    title = soup.find('h1', {'class': 'article'}).text.strip()
+    object_news = soup.find('div', id='article-text').find_all('p')
+    news = []
+    for p in object_news:
+        news.append(p.text)
+    return title, news
+
+
+def write_news(tuple):
+    name = 'News for OLEGA.doc'
+
+    with open(name, 'a') as f:
+        f.write(tuple[0])
+        f.write('\n')
+        for el in tuple[1]:
+            f.write(el)
+        f.write('\n\n\n')
+
+
 def main():
     all_links = get_all_links(get_html(site))
 
     for link in all_links:
-        print(link)
+        write_news(get_links_data(get_html(link)))
 
 
 if __name__ == '__main__':
